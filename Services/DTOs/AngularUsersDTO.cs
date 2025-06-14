56a8eb96d94c1d485c57b6a5c4caad9e388909cd
Harihar Binwal <72184327+binwalharihar@users.noreply.github.com>
2025-06-14 19:25:28 +0530

    commit

--- Services/DTOs/AngularUsersDTO.cs
+++ Services/DTOs/AngularUsersDTO.cs
@@ -10,5 +10,6 @@ namespace Services.DTOs
         public string name { get; set; }
 
         public string Password { get; set; }
+
     }
 }
--- Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c62c092d5a7f55484ddebee1b264f9fe1763bfb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
